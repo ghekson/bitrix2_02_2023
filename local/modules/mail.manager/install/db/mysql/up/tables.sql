e85f6dee8ece6bd9413644d0bbb1dd558d313b50
David <51459488+ghekson@users.noreply.github.com>
2023-02-15 13:54:18 +0300

    БД

--- local/modules/mail.manager/install/db/mysql/up/tables.sql
+++ local/modules/mail.manager/install/db/mysql/up/tables.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `y_emails` (
-                    `ID` int(11) NOT NULL AUTO_INCREMENT,
-                    `NAME` varchar(255) NOT NULL,
-                    `EMAIL` varchar(255) NOT NULL,
-                    PRIMARY KEY(`ID`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+`ID` int(11) NOT NULL AUTO_INCREMENT,
+`EMAIL` varchar(255) NOT NULL,
+PRIMARY KEY(`ID`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
